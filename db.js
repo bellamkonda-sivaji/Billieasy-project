a67e8f9572d507d366fe6ec48ab624ae4193106b
sivaji-bellamkonda <sivajibellamkonda99@gmail.com>
2021-12-02 12:46:30 +0530

    billieasy project

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const Pool=require("pg").Pool;
+const Pool = require("pg").Pool;
 
-const pool=new Pool({
+const pool = new Pool({
     user:"postgres",
     password:"database",
     database:"database2",
@@ -8,4 +8,4 @@ const pool=new Pool({
     port:5432
 });
 
-module.exports=pool;
+module.exports =pool;
